08de8f82f031a0a780ada1bee1ced0f4ac365718
nathaniel.a.solomon <>
2021-09-04 00:27:53 -0400

    sharing vars

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -6,3 +6,5 @@ mkdir $HOME/secrets
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output $HOME/secrets/sec_encryption.json sec_encryption.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/sec_encryption.yml sec_encryption.yml.gpg
