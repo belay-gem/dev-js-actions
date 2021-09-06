9cfcb3a988bc6fdbac13e0745014d153a856711a
nathaniel.a.solomon <>
2021-09-06 13:42:27 -0400

    test...

--- index.js
+++ index.js
@@ -1,32 +1,104 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { execSync } = require('child_process');
+const delay =  require('delay');
+var exec = require('child_process').exec, child;
+const envFile = require('dotenv');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('');
-    const topics = core.getInput('topics');
-    const tags = core.getInput('tags');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    console.log(`${process.env.logLevel} - This is logLevel`)
-    console.log(`${process.env.tags} - This is tags`)
-    console.log(`${process.env.topics} - This is topics`)
-    console.log(`${process.env.kafka} - This is kafka`)
-    console.log("Testing..........................")
-    console.log(process.env.DOC_USER_NAME)
-    console.log(`${process.env.DOCKER_PASSWORD}`)
-    console.log(`${process.env.AWS_REGION}`)
-    console.log("Testing.........................")
+var nope = ()=>{
+  try {
+      const nameToGreet = core.getInput('');
+      const topics = core.getInput('topics');
+      const tags = core.getInput('tags');
+      console.log(`Hello ${nameToGreet}!`);
+      const time = (new Date()).toTimeString();
+      core.setOutput("time", time);
+      console.log(`${process.env.DOCKER_PASSWORD}`)
+      console.log(`${process.env.AWS_REGION}`)
+      console.log(`${process.env.DOC_USER_NAME}`)
+      console.log("Testing.........................")
+    } catch (error) {
+      core.setFailed(error.message);
+    }
 
-    //console.log(`${process.env.DEPLOY_ENV}`);
-    //console.log(topics);
-    //console.log(tags)
-    //console.log(`${github.event.inputs.tags }`)
-    //console.log(`${github.event.inputs.kafka }`)
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-  } catch (error) {
-    core.setFailed(error.message);
+}
+// var decryption = async() =>{
+// // require('child_process').exec(`
+// //   gpg --quiet --batch --yes --decrypt --passphrase="Doma##@@" \
+// //   --output .env.encryption .env.encryption.gpg
+// // `)
+// //console.log("wow")
+// //await new Promise(resolve => setTimeout(resolve, 3000)); // 3 sec
+// //console.log("noppe")
+// }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// exec(`
+//     gpg --quiet --batch --yes --decrypt --passphrase="Doma##@@" \
+//     --output .env.encryption .env.encryption.gpg
+//   `,
+//     function (error, stdout, stderr) {
+//         console.log('stdout: ' + stdout);
+//         console.log('stderr: ' + stderr);
+//         if (error !== null) {
+//              console.log('exec error: ' + error);
+//         }
+// });
+
+// exec(`
+//     gpg --quiet --batch --yes --decrypt --passphrase="Doma##@@" \
+//     --output .env.encryption .env.encryption.gpg
+//   `,
+//     function (error, stdout, stderr) {
+//         console.log('stdout: ' + stdout);
+//         console.log('stderr: ' + stderr);
+//         if (error !== null) {
+//              console.log('exec error: ' + error);
+//         }
+// });
+// sleep(2000);
+//
+// execSync(`
+//     gpg --quiet --batch --yes --decrypt --passphrase="Doma##@@" \
+//     --output .env.encryption .env.encryption.gpg
+//   `);
+// sleep(5000);
+// nope();
+
+async function task1(input){
+  execSync(`
+      gpg --quiet --batch --yes --decrypt --passphrase="Doma##@@" \
+      --output .env .env.encryption.gpg
+    `);
+    console.log("one: "+input);
+    envFile.config();
+
+
+    }
+    async function task2(input){
+       // has more functions that do other stuff
+       console.log("two: "+input);
+
+    }
+
+    function task3(input){
+       // this code should only be executed after both task 1 and 2 finish
+       console.log("three: "+input)
+    }
+
+    async function main(input1, input2){
+      try{
+        await Promise.all(
+            [task1(input1)]
+        );
+        nope();
+        task2(input2);
+    }catch(err){
+      console.log("error");
+      console.log(err);
+    }
   }
+
+    main("input1", "input2");
