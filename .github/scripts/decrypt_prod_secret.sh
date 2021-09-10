62da8fe8e6dd7a1e6e4e173b56f0535d647f6ecd
nathaniel.a.solomon <>
2021-09-10 16:20:01 -0400

    testing

---
+++ .github/scripts/decrypt_prod_secret.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+# gpg --quiet --batch --yes --decrypt --passphrase="$DEV_ENCRYPTION_KEY21" \
+# --output $HOME/secrets/sec_encryption.json sec_encryption.json.gpg
+# gpg --pinentry loopback --quiet --batch --yes --decrypt --passphrase="$DEV_KEY" \
+# --output $HOME/.env.dev.encryption .env.dev.encryption.gpg
+# gpg --pinentry loopback --quiet --batch --yes --decrypt --passphrase="$STAGING_KEY" \
+# --output $HOME/.env.staging.encryption .env.staging.encryption.gpg
+# gpg --pinentry loopback --quiet --batch --yes --decrypt --passphrase="$PROD_KEY" \
+# --output $HOME/.env.prod.encryption .env.prod.encryption.gpg
+gpg --version
+# gpg  --quiet --batch --yes --decrypt --passphrase="$DEV_KEY" \
+# --output $HOME/.env.dev.encryption .env.dev.encryption.gpg
+# gpg  --quiet --batch --yes --decrypt --passphrase="$STAGING_KEY" \
+# --output $HOME/.env.staging.encryption .env.staging.encryption.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$PROD_KEY" \
+--output $HOME/.env.prod.encryption .env.prod.encryption.gpg
