ff1cd77aa6632bcb31c4814e07c46e4f9a8fcf30
nathaniel.a.solomon <>
2021-09-09 23:25:28 -0400

    addition of secrets

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,7 +4,7 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$DEV_ENCRYPTION_KEY21" \
 --output $HOME/secrets/sec_encryption.json sec_encryption.json.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$DEV_ENCRYPTION_KEY21" \
 --output $HOME/.env.encryption .env.encryption.gpg
